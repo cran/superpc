cc7b1d571f6dda55dd7f0ad6dbe40379c0590ce0
Rob Tibshirani <tibs@stat.stanford.edu>
2004-10-22 00:00:00 +0000

    version 1.01

--- R/superpc.plotcv.R
+++ R/superpc.plotcv.R
@@ -1,24 +1,29 @@
-superpc.plotcv<-
-  
-  function (object,  smooth=TRUE, smooth.df=10, ...) 
+superpc.plotcv=
+function (object, smooth = TRUE, smooth.df = 6, ...)
 {
+    scor = object$scor.preval
+k=nrow(scor)
+    if (smooth) {
+        for (j in 1:nrow(scor)) {
+         if(is.null(smooth.df)){
+               junk=smooth.spline(object$th, scor[j, ])
+               scor[j, ] = predict(junk,object$th)$y
+              }
+            if(!is.null(smooth.df)){
+             junk=smooth.spline(object$th, scor[j, ], df=smooth.df)
+            scor[j, ] =predict(junk,object$th)$y
+             }
+        }
+    }
+    ymax = max(scor, qchisq(0.95, nrow(scor)))
+    matplot(object$th, t(scor), xlab = "Threshold", ylab = "Likelihood ratio test statistic",
+        ylim = c(0, ymax), lty=rep(1,k))
+    matlines(object$th, t(scor), lty=rep(1,k), ...)
 
-  scor=object$scor.preval
 
-  if(smooth){
-    for(j in 1:nrow(scor)){
-      scor[j,]=smooth.spline(object$th, scor[j,],df=smooth.df)$y
-    }}
-  
-  ymax=max(scor,qchisq(.95,nrow(scor)))
-
-  matplot(object$th, t(scor), xlab = "Threshold", 
-          ylab = "Likelihood ratio test statistic", ylim=c(0,ymax))
-  matlines(object$th, t(scor), ...)
-
-  for(j in 1:nrow(scor)){
-    abline(h=qchisq(.95,j),lty=2,col=j)
-  }
-
+    for (j in 1:k) {
+        abline(h = qchisq(0.95, j), lty = 2, col = j)
+    }
 }
+
 
